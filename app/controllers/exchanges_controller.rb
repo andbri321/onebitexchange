8bfa51a1e87c28d8851a3e38f956be4039d0083b
andbri321 <andbri321@gmail.com>
2019-06-10 21:20:29 -0300

    ss

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -6,4 +6,5 @@ class ExchangesController < ApplicationController
     value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
     render json: {"value": value}
   end
+  
 end
